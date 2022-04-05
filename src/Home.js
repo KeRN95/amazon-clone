688ffc3d3d266e829e71be95aaded307b70051ac
erind <erind.buzani@outlook.com>
2022-04-05 22:13:32 +0200

    intial commit

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://i0.wp.com/static.makeuseof.com/wp-content/uploads/2020/06/amazon-prime-video-watch-party-994x400.jpg"
+        ></img>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBqi_LaBwDddFdFYuB0AC0ZGCHLaeF2j5M",
+    authDomain: "challenge-1fa21.firebaseapp.com",
+    projectId: "challenge-1fa21",
+    storageBucket: "challenge-1fa21.appspot.com",
+    messagingSenderId: "433125243366",
+    appId: "1:433125243366:web:2cacc8930fae408205fe27",
+    measurementId: "G-RRBC7Q52GG"
+  };
